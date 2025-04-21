dd05c55ea973572f9304c116c449c55fb3b79f60
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-04-22 01:07:24 +0800

    Add test model CertificateInfo validation

--- src/TestPfxCertificate/TestPfxCertificate.cs
+++ src/TestPfxCertificate/TestPfxCertificate.cs
@@ -1,8 +1,8 @@
-﻿using System.Security.Cryptography.X509Certificates;
-
 namespace TirsvadCLI.PfxCertificate.Tests;
 
+using System.Security.Cryptography.X509Certificates;
 using TirsvadCLI.PfxCertificate;
+using TirsvadCLI.PfxCertificate.Model;
 
 [TestClass]
 public class CertificateTests
@@ -123,5 +123,104 @@ public class CertificateTests
         {
             store.Remove(cert);
         }
+    }
+}
+
+[TestClass]
+public class CertificateInfoValidationTests
+{
+    [TestMethod]
+    public void IsValidCertificatePath_ShouldReturnExpectedResults()
+    {
+        Assert.AreEqual(1, CertificateInfo.IsValidCertificatePath(null));
+        Assert.AreEqual(1, CertificateInfo.IsValidCertificatePath(string.Empty));
+        Assert.AreEqual(2, CertificateInfo.IsValidCertificatePath("NonExistentPath"));
+        Assert.AreEqual(0, CertificateInfo.IsValidCertificatePath(Environment.CurrentDirectory));
+    }
+
+    [TestMethod]
+    public void IsValidCertificateFilename_ShouldReturnExpectedResults()
+    {
+        Assert.AreEqual(1, CertificateInfo.IsValidCertificateFilename(null));
+        Assert.AreEqual(1, CertificateInfo.IsValidCertificateFilename(string.Empty));
+        Assert.AreEqual(2, CertificateInfo.IsValidCertificateFilename("filename"));
+        Assert.AreEqual(3, CertificateInfo.IsValidCertificateFilename("filename.txt"));
+        Assert.AreEqual(4, CertificateInfo.IsValidCertificateFilename(new string('a', 256) + ".pfx"));
+        Assert.AreEqual(0, CertificateInfo.IsValidCertificateFilename("valid.pfx"));
+    }
+
+    [TestMethod]
+    public void IsValidCertificatePassword_ShouldReturnExpectedResults()
+    {
+        Assert.AreEqual(1, CertificateInfo.IsValidCertificatePassword(null));
+        Assert.AreEqual(1, CertificateInfo.IsValidCertificatePassword(string.Empty));
+        Assert.AreEqual(2, CertificateInfo.IsValidCertificatePassword("short"));
+        Assert.AreEqual(3, CertificateInfo.IsValidCertificatePassword(new string('a', 256)));
+        Assert.AreEqual(0, CertificateInfo.IsValidCertificatePassword("ValidPassword123"));
+    }
+
+    [TestMethod]
+    public void IsValidCertSubject_ShouldReturnExpectedResults()
+    {
+        Assert.AreEqual(1, CertificateInfo.IsValidCertSubject(null));
+        Assert.AreEqual(1, CertificateInfo.IsValidCertSubject(string.Empty));
+        Assert.AreEqual(2, CertificateInfo.IsValidCertSubject(new string('a', 256)));
+        Assert.AreEqual(0, CertificateInfo.IsValidCertSubject("ValidSubject"));
+    }
+
+    [TestMethod]
+    public void IsValidCertOrganization_ShouldReturnExpectedResults()
+    {
+        Assert.AreEqual(1, CertificateInfo.IsValidCertOrganization(null));
+        Assert.AreEqual(1, CertificateInfo.IsValidCertOrganization(string.Empty));
+        Assert.AreEqual(2, CertificateInfo.IsValidCertOrganization(new string('a', 256)));
+        Assert.AreEqual(0, CertificateInfo.IsValidCertOrganization("ValidOrganization"));
+    }
+
+    [TestMethod]
+    public void IsValidCertOrganizationUnit_ShouldReturnExpectedResults()
+    {
+        Assert.AreEqual(1, CertificateInfo.IsValidCertOrganizationUnit(null));
+        Assert.AreEqual(1, CertificateInfo.IsValidCertOrganizationUnit(string.Empty));
+        Assert.AreEqual(2, CertificateInfo.IsValidCertOrganizationUnit(new string('a', 256)));
+        Assert.AreEqual(0, CertificateInfo.IsValidCertOrganizationUnit("ValidUnit"));
+    }
+
+    [TestMethod]
+    public void IsValidCertLocality_ShouldReturnExpectedResults()
+    {
+        Assert.AreEqual(1, CertificateInfo.IsValidCertLocality(null));
+        Assert.AreEqual(1, CertificateInfo.IsValidCertLocality(string.Empty));
+        Assert.AreEqual(2, CertificateInfo.IsValidCertLocality(new string('a', 256)));
+        Assert.AreEqual(0, CertificateInfo.IsValidCertLocality("ValidLocality"));
+    }
+
+    [TestMethod]
+    public void IsValidCertState_ShouldReturnExpectedResults()
+    {
+        Assert.AreEqual(1, CertificateInfo.IsValidCertState(null));
+        Assert.AreEqual(1, CertificateInfo.IsValidCertState(string.Empty));
+        Assert.AreEqual(2, CertificateInfo.IsValidCertState(new string('a', 256)));
+        Assert.AreEqual(0, CertificateInfo.IsValidCertState("ValidState"));
+    }
+
+    [TestMethod]
+    public void IsValidCertCountry_ShouldReturnExpectedResults()
+    {
+        Assert.AreEqual(1, CertificateInfo.IsValidCertCountry(null));
+        Assert.AreEqual(1, CertificateInfo.IsValidCertCountry(string.Empty));
+        Assert.AreEqual(2, CertificateInfo.IsValidCertCountry(new string('a', 256)));
+        Assert.AreEqual(0, CertificateInfo.IsValidCertCountry("US"));
+    }
+
+    [TestMethod]
+    public void IsValidCertValidityYears_ShouldReturnExpectedResults()
+    {
+        Assert.AreEqual(1, CertificateInfo.IsValidCertValidityYears((string)null));
+        Assert.AreEqual(1, CertificateInfo.IsValidCertValidityYears(string.Empty));
+        Assert.AreEqual(2, CertificateInfo.IsValidCertValidityYears("InvalidNumber"));
+        Assert.AreEqual(3, CertificateInfo.IsValidCertValidityYears(0));
+        Assert.AreEqual(4, CertificateInfo.IsValidCertValidityYears(21));
+        Assert.AreEqual(0, CertificateInfo.IsValidCertValidityYears(5));
     }
 }
